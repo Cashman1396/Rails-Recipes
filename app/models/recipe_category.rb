e7628f80d33719a822e04241ca801da81382b38f
chris_ashman <chrisashman96@gmail.com>
2020-06-25 23:53:08 -0400

    added code to views

--- app/models/recipe_category.rb
+++ app/models/recipe_category.rb
@@ -1,2 +1,9 @@
 class RecipeCategory < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :category
+
+  validates :category_id, uniqueness: { scope: :recipe_id, message: "should have one type of category per recipe"}
+end
+
+    
 end
